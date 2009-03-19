8a1cd6ae2904eeb56bf0c16fab71cdff9fe41322
Daniel Lehmann <leh@l3h.de>
2009-03-19 17:58:30 +0100

    Spicycode-rcov compatibility fix. Now httparty and rcov are using the same monkey patching for the broken rexml-stuff

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -154,22 +154,77 @@ end
 
 # 1.8.6 has mistyping of transitive in if statement
 require "rexml/document"
-module REXML #:nodoc:
-  class Document < Element #:nodoc:
-    def write( output=$stdout, indent=-1, transitive=false, ie_hack=false )
-      if xml_decl.encoding != "UTF-8" && !output.kind_of?(Output)
-        output = Output.new( output, xml_decl.encoding )
-      end
-      formatter = if indent > -1
-          if transitive
-            REXML::Formatters::Transitive.new( indent, ie_hack )
-          else
-            REXML::Formatters::Pretty.new( indent, ie_hack )
-          end
-        else
-          REXML::Formatters::Default.new( ie_hack )
+
+if (RUBY_VERSION == "1.8.6" || RUBY_VERSION == "1.8.7") && defined? REXML::Formatters::Transitive
+    class REXML::Document
+        remove_method :write rescue nil
+        def write( output=$stdout, indent=-1, trans=false, ie_hack=false )
+            if xml_decl.encoding != "UTF-8" && !output.kind_of?(Output)
+                output = Output.new( output, xml_decl.encoding )
+            end
+            formatter = if indent > -1
+                #if trans
+                    REXML::Formatters::Transitive.new( indent )
+                #else
+                #    REXML::Formatters::Pretty.new( indent, ie_hack )
+                #end
+            else
+                REXML::Formatters::Default.new( ie_hack )
+            end
+            formatter.write( self, output )
         end
-      formatter.write( self, output )
     end
-  end
+
+    class REXML::Formatters::Transitive
+        remove_method :write_element rescue nil
+        def write_element( node, output )
+            output << "<#{node.expanded_name}"
+
+            node.attributes.each_attribute do |attr|
+                output << " "
+                attr.write( output )
+            end unless node.attributes.empty?
+
+            if node.children.empty?
+                output << "/>"
+            else
+                output << ">"
+                # If compact and all children are text, and if the formatted output
+                # is less than the specified width, then try to print everything on
+                # one line
+                skip = false
+                @level += @indentation
+                node.children.each { |child|
+                    write( child, output )
+                }
+                @level -= @indentation
+                output << "</#{node.expanded_name}>"
+            end
+            output << "\n"
+            output << ' '*@level
+        end
+    end
+
 end
+
+
+
+#module REXML #:nodoc:
+#  class Document < Element #:nodoc:
+#    def write( output=$stdout, indent=-1, transitive=false, ie_hack=false )
+#      if xml_decl.encoding != "UTF-8" && !output.kind_of?(Output)
+#        output = Output.new( output, xml_decl.encoding )
+#      end
+#      formatter = if indent > -1
+#          if transitive
+#            REXML::Formatters::Transitive.new( indent, ie_hack )
+#          else
+#            REXML::Formatters::Pretty.new( indent, ie_hack )
+#          end
+#        else
+#          REXML::Formatters::Default.new( ie_hack )
+#        end
+#      formatter.write( self, output )
+#    end
+#  end
+#end
